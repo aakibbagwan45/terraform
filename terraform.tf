e90fe81daadeb7a6e80e717f80e1e4ace76133bd
aakibbagwan45 <aakibbagwan45@gmail.com>
2022-10-15 17:59:51 +0000

    removed changes

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,5 @@ terraform {
   bucket   =  "terraform-jenkins-aakibbagwanit"
   key      =  "aws/terraform.tfstate"
   region   =  "ap-south-1"
-  access_key = "echo ($AWS_ACCESS_KEY_ID)"
-  secret_key = "echo ($AWS_SECRET_ACCESS_KEY)"
   }
 }
