094ade63d311e8709f592de01ad2098d294c5864
aakibbagwan45 <aakibbagwan45@gmail.com>
2022-10-15 18:49:56 +0000

    reverse changes

--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
-terraform {
-      required_providers {
-         aws = {
-         source = "hashicorp/aws"
-         version = "= 3.74.2"
-         region = "ap-south-1"
-        }
-     }
-  }
+provider "aws" {
+  region = "ap-south-1"
+}
