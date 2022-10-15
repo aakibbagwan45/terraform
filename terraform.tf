377436156c15b887269095ccaa4c53d2f76ffd1e
aakibbagwan45 <aakibbagwan45@gmail.com>
2022-10-15 17:39:27 +0000

    fix applied

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,6 @@ terraform {
   bucket   =  "terraform-jenkins-aakibbagwanit"
   key      =  "aws/terraform.tfstate"
   region   =  "ap-south-1"
-  shared_config_files      = ["/root/.aws/config"]
-  shared_credentials_files = ["/root/.aws/credentials"]
+  shared_credentials_files = ["/home/jenkins/credentials"]
   }
 }
