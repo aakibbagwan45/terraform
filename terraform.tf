6b30bd7d80f206725fc51f0b6c09a1328252c5f7
aakibbagwan45 <aakibbagwan45@gmail.com>
2022-10-15 17:55:06 +0000

    fix applied 3

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   bucket   =  "terraform-jenkins-aakibbagwanit"
   key      =  "aws/terraform.tfstate"
   region   =  "ap-south-1"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  access_key = "echo ($AWS_ACCESS_KEY_ID)"
+  secret_key = "echo ($AWS_SECRET_ACCESS_KEY)"
   }
 }
