fca7f8a9d10a28834941b93f3d878def1cfb239b
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-16 09:13:35 +0000

    added updated config

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
-provider "s3" {
+provider "aws" {
   region = "ap-south-1"
+  shared_config_files      = ["/root/.aws/config"]
+  shared_credentials_files = ["/root/.aws/credentials"]
+  profile                  = "customprofile"
 }
--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,5 @@ terraform {
   key = "jenkins-iam"
   region = "ap-south-1"
   dynamodb_table = "state-locking"
-  access_key = {$AWS_ACCESS_KEY_ID}
-  secret_key = {$AWS_SECRET_ACCESS_KEY}
   }
 }
