51203c2237e9e2b53661779e17a26d580080e7fb
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-16 09:24:53 +0000

    added aws profile

--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  shared_config_files      = ["/root/.aws/config"]
+  shared_credentials_files = ["/root/.aws/credentials"]
+  profile                  = "customprofile"
 }
