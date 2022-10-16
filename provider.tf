611ec671217f60eb58edd63bb914d890b708d650
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-16 09:17:03 +0000

    remove cred

--- provider.tf
+++ provider.tf
@@ -1,6 +1,3 @@
 provider "aws" {
   region = "ap-south-1"
-  shared_config_files      = ["/root/.aws/config"]
-  shared_credentials_files = ["/root/.aws/credentials"]
-  profile                  = "customprofile"
 }
