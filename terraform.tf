fe0a649e8e018cb2dec0403ef6d134d3c01e40e0
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-16 09:03:45 +0000

    added keys vars

--- terraform.tf
+++ terraform.tf
@@ -4,5 +4,7 @@ terraform {
   key = "jenkins-iam"
   region = "ap-south-1"
   dynamodb_table = "state-locking"
+  access_key = var.${AWS_ACCESS_KEY_ID}
+  secret_key = var.${AWS_SECRET_ACCESS_KEY}
   }
 }
