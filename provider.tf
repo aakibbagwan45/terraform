96ee91795c92abb4d406cad28c220144e7fe0044
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-22 15:06:42 +0000

    update variable

--- provider.tf
+++ provider.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  shared_config_files      = ["/aws/config"]
-  shared_credentials_files = ["/aws/cred"]
-  profile                  = "awscred" 
-  region		 = "ap-south-1"
+  profile                  = "${var.awscred}"
+  region		 = "${var.region}"
 }
