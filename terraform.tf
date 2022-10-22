879aa11075fe2ff80d5c11963b5e08ad9c82cd38
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-22 16:08:35 +0000

    S3 bucket changed

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,8 @@ terraform {
    bucket = "myjenkinslab-terraform"
    key = "jenkins-iam"
    region = "ap-south-1"
+   shared_credentials_file = "~/.aws/credentials"
+   profile               = "awscred"
    dynamodb_table = "state-locking"
   }
 }
