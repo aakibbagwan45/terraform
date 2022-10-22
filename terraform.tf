88065dd316d0db9a29d781e519c702ffc68fe37f
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-22 16:11:28 +0000

    path changes

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
    bucket = "myjenkinslab-terraform"
    key = "jenkins-iam"
    region = "ap-south-1"
-   shared_credentials_file = "~/.aws/credentials"
+   shared_credentials_file = "/aws/cred"
    profile               = "awscred"
    dynamodb_table = "state-locking"
   }
