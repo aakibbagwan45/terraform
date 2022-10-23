c052b76b770155e084a784a8f5ef7c9cf04c5063
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-23 21:41:12 +0000

    added name path

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
-   key = "nikita/terraform.tfstate"
+   key = "aws_iam_user.user.name/terraform.tfstate"
    region = "ap-south-1"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
