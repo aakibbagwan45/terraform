716c7ff1f51052bd5db694c040750777dc52f6f8
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-23 15:00:38 +0000

    changed state location to firdous

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
-   key = "kaif/terraform.tfstate"
+   key = "firdous/terraform.tfstate"
    region = "ap-south-1"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
