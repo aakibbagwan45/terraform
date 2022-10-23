6f8936ee5cdd14852eab62aa3253b6c3f10d71db
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-23 14:37:26 +0000

    changed state location to kaif

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
-   key = "jenkins-iam"
+   key = "kaif/"
    region = "ap-south-1"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
