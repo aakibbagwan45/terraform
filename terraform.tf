aca5a5e2413454bbb635fd58f7ec5edfac96573d
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-23 21:29:07 +0000

    adding location for nikita

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
-   key = "sahil/terraform.tfstate"
+   key = "nikita/terraform.tfstate"
    region = "ap-south-1"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
