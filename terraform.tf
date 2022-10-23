c61fcfb293cb9ee3aee813b0aefc1df7389008ea
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-23 21:25:22 +0000

    adding location for sahil

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
-   key = "var.name/terraform.tfstate"
+   key = "sahil/terraform.tfstate"
    region = "ap-south-1"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
