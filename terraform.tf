83d92a4a9c52f8b2fdd270da8fd27eec6aa5a30b
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-23 15:06:28 +0000

    changed state location to kiran

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
-   key = "firdous/terraform.tfstate"
+   key = "kiran/terraform.tfstate"
    region = "ap-south-1"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
