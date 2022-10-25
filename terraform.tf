109fd96c3f948856f957706f5094ea3a249138e2
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-25 07:55:36 +0000

    removed key paramter

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
-   key = "aws_iam_user.user.name/terraform.tfstate"
    region = "ap-south-1"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
