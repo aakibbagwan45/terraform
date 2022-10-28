930900f7f857a668f93fa79dfed615e26cb72dda
root <root@ip-172-31-39-163.ap-south-1.compute.internal>
2022-10-28 12:19:29 +0000

    added privious or working parameter

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
    bucket = "myjenkinslab-terraform"
    region = "ap-south-1"
+   key = "aws_iam_user.user.name/terraform.tfstate"
    shared_credentials_file = "/aws/cred"
    profile               = "awscred"
    dynamodb_table = "state-locking"
